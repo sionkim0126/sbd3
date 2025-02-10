0a5b5185a6304c7e9766b35bcfc7546c611582c3
sionkim0126 <kimsion000@naver.com>
2025-02-10 17:33:28 +0900

    git저장소 생성 및 gradle 기본 의존성 설정

---
+++ src/main/java/sbd/example/Sbd3Application.java
@@ -0,0 +1,11 @@
+package sbd.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbd3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Sbd3Application.class, args);
+    }
+}
