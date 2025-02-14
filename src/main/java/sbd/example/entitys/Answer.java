b5ac324b9157f07b82d8a7c9c1d63c3b26438f85
sionkim0126 <kimsion000@naver.com>
2025-02-14 19:37:28 +0900

    Entity생성 및 h2데이터베이스 설정

---
+++ src/main/java/sbd/example/entitys/Answer.java
@@ -0,0 +1,24 @@
+package sbd.example.entitys;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    @Column( columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
---
+++ src/main/java/sbd/example/entitys/Question.java
@@ -0,0 +1,31 @@
+package sbd.example.entitys;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "question")
+public class Question {
+    //id 속성에 적용한 @Id 애너테이션은 id 속성을 기본키로 지정한다.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //번호 자동 생성 후 1씩 증가
+    private Integer id;
+
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column( columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
